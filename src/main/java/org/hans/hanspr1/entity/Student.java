6bf57d9a6878abb827eedaebf58fbdf0f09d37cf
Hans-git-std <hans3114@gmail.com>
2025-09-05 17:26:33 +0530

    Changes are done one teacher and Student class

--- src/main/java/org/hans/hanspr1/entity/Student.java
+++ src/main/java/org/hans/hanspr1/entity/Student.java
@@ -1,5 +1,11 @@
 package org.hans.hanspr1.entity;
 
-public class Student {
+import jakarta.annotation.sql.DataSourceDefinition;
 
+import java.util.List;
+//@Data
+public class Student {
+    private int roll;
+    private String name;
+    private List<Teacher> subject;
 }
---
+++ src/main/java/org/hans/hanspr1/entity/Teacher.java
@@ -0,0 +1,7 @@
+package org.hans.hanspr1.entity;
+
+public class Teacher {
+    private int id;
+    private String name;
+    private String subject;
+}
